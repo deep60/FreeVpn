1b3fb9b273011193eadee200b4a3845453e81aa3
deep60 <deep6060p@gmail.com>
2025-01-24 10:06:37 +0530

    Initial Commit

---
+++ FreeVPN/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FreeVPN
+//
+//  Created by P Deepanshu on 24/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FreeVPN/FreeVPNApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreeVPNApp.swift
+//  FreeVPN
+//
+//  Created by P Deepanshu on 24/01/25.
+//
+
+import SwiftUI
+
+@main
+struct FreeVPNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
