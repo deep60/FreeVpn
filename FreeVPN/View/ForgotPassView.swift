f9fbcdd072fe48997058b0ac10fd127011e100d4
deep60 <deep6060p@gmail.com>
2025-01-24 23:46:45 +0530

    first auth view

---
+++ FreeVPN/View/ForgotPassView.swift
@@ -0,0 +1,18 @@
+//
+//  ForgotPassView.swift
+//  FreeVPN
+//
+//  Created by P Deepanshu on 24/01/25.
+//
+
+import SwiftUI
+
+struct ForgotPassView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ForgotPassView()
+}
---
+++ FreeVPN/View/VerificationsView.swift
@@ -0,0 +1,18 @@
+//
+//  VerificationsView.swift
+//  FreeVPN
+//
+//  Created by P Deepanshu on 24/01/25.
+//
+
+import SwiftUI
+
+struct VerificationsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VerificationsView()
+}
